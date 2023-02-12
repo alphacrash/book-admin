12b308588371326d1e5b660dc67c767b298030b8
SUD <sudhansu.ace@gmail.com>
2023-02-12 23:41:38 +0530

    Initial Setup

---
+++ src/main/java/com/alphacrash/bookadmin/BookAdminApplication.java
@@ -0,0 +1,13 @@
+package com.alphacrash.bookadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAdminApplication.class, args);
+	}
+
+}
